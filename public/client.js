ae3c7210dadda6ecfcf0c292347e5f4a9b3fdaeb
Ken <ken.hanner@gmail.com>
2022-04-18 11:26:29 -0500

    client.js setup, add button

---
+++ public/client.js
@@ -0,0 +1,7 @@
+console.log('Client code running...');
+
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
